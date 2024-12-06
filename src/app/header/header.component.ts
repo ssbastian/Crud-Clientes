78ce2c19308f0958e6c71f0f454b829bb3febbbc
Sebastian <tatan873@gmail.com>
2024-12-05 19:32:55 -0500

    add proyecto front

---
+++ src/app/header/header.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-header',
+  imports: [],
+  templateUrl: './header.component.html',
+  styleUrl: './header.component.css'
+})
+export class HeaderComponent {
+  title : string = 'Proyecto de clase' 
+  /* hacer una interpolacion con title */
+}
